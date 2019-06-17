6203a87d29a1ff2f72b7ee166854ce29776d03b0
unknown <gabriela_o_da-silva@saojose.sc.senai.br>
2019-06-17 08:44:04 -0300

    modulo 3 aula 2

---
+++ mod3/aulas/1/todo.js
@@ -0,0 +1,6 @@
+var listElement = document.querySelector("#app ul")
+var inputElement = document.querySelector("#app input")
+var buttonElement = document.querySelector("#app button")
+console.log(listElement);
+console.log(inputElement);
+console.log(buttonElement);
